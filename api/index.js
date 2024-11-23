35ca5508d871434dc60383487a70b6392c4fc211
hernan@ardentcreative.com <hernan@mobomo.com>
2024-11-23 17:43:32 -0300

    Versioning untracked files

---
+++ api/index.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+const router = express.Router();
+var baseUrl = "https://apidev.mercantilandina.com.ar";
+const subsKey = "d4f43f902b4049218451d78eb5966156";
+const axios = require("axios");
+
+app.listen(8000, () => {
+  console.log('App running on PORT 3000 a')
+});
+
+const data = {
+  'username': 'GASLUTST',
+  'password': 'gaslu2024',
+  'grant_type': 'password',
+  'client_id': 'api-clientes-login',
+}
+
+
+const headers = {
+  'Ocp-Apim-Subscription-Key': subsKey,
+  'Access-Control-Allow-Origin': '*',
+  'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+
+router.get("/credenciales", async (req, res) => {
+  try {
+    const response = await axios.post(baseUrl + "/credenciales/v2/", data, { headers });
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+app.use(router);
+
+module.exports = app;
