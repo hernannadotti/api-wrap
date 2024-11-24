f44110dd28ac237d4611d434603507d3951131c9
hernan@ardentcreative.com <hernan@mobomo.com>
2024-11-23 22:57:44 -0300

    implement cors on express

--- api/index.js
+++ api/index.js
@@ -4,10 +4,13 @@ const router = express.Router();
 var baseUrl = "https://apidev.mercantilandina.com.ar";
 const subsKey = "d4f43f902b4049218451d78eb5966156";
 const axios = require("axios");
+const cors = require('cors');
 
 app.listen(8000, () => {
   console.log('App running on PORT 3000')
 });
+
+app.use(cors())
 
 
 
