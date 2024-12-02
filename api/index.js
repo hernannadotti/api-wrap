5555eed4f870279e8de7f48f4916b8464cae6190
hernan@ardentcreative.com <hernan@mobomo.com>
2024-12-02 07:09:59 -0300

    fix cors

--- api/index.js
+++ api/index.js
@@ -8,9 +8,32 @@ const cors = require('cors');
 var bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
+const bearerToken = require('express-bearer-token');
+app.use(bearerToken());
+
+// app.use(function (req, res) {
+//   res.send('Token '+req?.token);
+// });
+
+app.use(bearerToken({
+  bodyKey: 'access_token',
+  queryKey: 'access_token',
+  headerKey: 'Bearer',
+  reqKey: 'token',
+  cookie: true, // by default is disabled
+}));
+
+app.use(bearerToken({
+  cookie: {
+    signed: true, // if passed true you must pass secret otherwise will throw error
+    secret: 'YOUR_APP_SECRET',
+    key: 'access_token' // default value
+  }
+}));
+
 
 app.listen(8000, () => {
-  console.log('App running on PORT 3000')
+  console.log('App running on PORT 3000');
 });
 
 const body = {
@@ -21,52 +44,35 @@ const body = {
 }
 
 
-const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200,
-  methods: 'GET, POST, PUT, DELETE, OPTIONS',
-}
-
 app.use(cors());
 
-const authToken = '';
 const headers = {
   'Ocp-Apim-Subscription-Key': subsKey,
   'Content-Type': 'application/x-www-form-urlencoded'
 }
 
-const headersGet = {
-  'Ocp-Apim-Subscription-Key': subsKey,
-  'Content-Type': 'application/x-www-form-urlencoded'
-}
 
-// get Token
-router.get("/credenciales", (req, res, next) => {
+router.get("/credenciales", (req, res) => {
   axios.post(`${baseUrl}/credenciales/v2`, body, {
     headers: headers
+  }).then(response => {
+    res.cookie('access_token', response.data.access_token , {maxAge: 9000000000, httpOnly: true, secure: true });
+    res.send(response.data);
+  }).catch(error => {
+    res.send(error);
   })
-    .then(response => {
-      // localStorage.setItem('token', response.data.access_token);
-      res.cookie('token', response.data.access_token);
-      res.send(response.data);
-    })
-    .catch(error => {
-      res.send(error);
-    })
-});
+})
 
 
 
 // Get Localidades
-router.get('/localidades/:q', (req, res) => {
+router.get('/localidades/:q',(req, res) => {
+  let token = req.cookies.access_token;
   axios.get(`${baseUrl}/generales/v1/localidades?q=${req.params.q}`, {
     headers: {
       'Ocp-Apim-Subscription-Key': subsKey,
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': `Bearer ${req.cookies.token}`,
-      'Accept': 'application/json',
-      'Allow-Control-Allow-Origin': '*'
-
+      'Authorization': `Bearer ${token}`
     }
   })
     .then(response => {
